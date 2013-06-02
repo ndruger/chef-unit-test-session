9745ea3f4f5677307424bf920466e27ca6aa33c2
ndruger <snowfullnight@gmail.com>
2013-06-03 05:47:31 +0900

    [feature] Add jenkins

--- Berksfile
+++ Berksfile
@@ -1,5 +1,7 @@
 site :opscode
 cookbook 'yum'
 cookbook 'rvm', github: 'fnichol/chef-rvm'
+cookbook 'java'
+cookbook 'jenkins', github: 'fnichol/chef-jenkins'
 
 
---
+++ site-cookbooks/disabled_iptables/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: disabled_iptables
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+service 'iptables' do
+	action [:disable, :stop]
+end
+
